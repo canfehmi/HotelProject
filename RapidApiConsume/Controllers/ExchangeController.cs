f609632902266282789ef2d6275cedf9573b2095
n.fehmi26 <n.fehmi26@gmail.com>
2025-01-07 00:39:56 +0300

    Added: RapidApi

---
+++ RapidApiConsume/Controllers/ExchangeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+
+namespace RapidApiConsume.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com15.p.rapidapi.com/api/v1/meta/getExchangeRates?base_currency=TRY"),
+                Headers =
+    {
+        { "x-rapidapi-key", "a6644f2004msh962ed9b018ea887p1889e4jsn794d88aa923a" },
+        { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.data.exchange_rates.ToList());
+            }
+        }
+    }
+}
