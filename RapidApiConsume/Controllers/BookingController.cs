144827719d0e548907a4a7a0de77a7dfc6abceef
n.fehmi26 <n.fehmi26@gmail.com>
2025-01-08 00:23:41 +0300

    Added: new api project (jwt)

---
+++ RapidApiConsume/Controllers/BookingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+using System.Net.Http.Headers;
+
+namespace RapidApiConsume.Controllers
+{
+    public class BookingController : Controller
+    {
+        public async Task< IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels?dest_id=-2092174&search_type=CITY&adults=1&children_age=0%2C17&room_qty=1&page_number=1&units=metric&temperature_unit=c&languagecode=en-us&currency_code=AED"),
+                Headers =
+    {
+        { "x-rapidapi-key", "a6644f2004msh962ed9b018ea887p1889e4jsn794d88aa923a" },
+        { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingApiViewModel>(body);
+                
+                return View(values.data.hotels.ToList());
+            }
+        }
+    }
+}
